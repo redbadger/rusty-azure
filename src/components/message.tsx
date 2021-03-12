ce8c1a46c58dc73209a82c7aa3b72eb0c74ad113
Pedro Martin <pedro.martin@red-badger.com>
2021-03-12 16:38:51 +0000

    harcoded script

---
+++ src/components/message.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  border-radius: 20px;
+  padding: 8px 15px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  position: relative;
+
+  &.yours {
+    margin-right: 25%;
+    background-color: #eee;
+
+    &:before {
+      content: '';
+      position: absolute;
+      z-index: 0;
+      bottom: 0;
+      left: -7px;
+      height: 20px;
+      width: 20px;
+      background: #eee;
+      border-bottom-right-radius: 15px;
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      z-index: 1;
+      bottom: 0;
+      left: -10px;
+      width: 10px;
+      height: 20px;
+      background: white;
+      border-bottom-right-radius: 10px;
+    }
+  }
+
+  &.mine {
+    align-self: end;
+    color: white;
+    margin-left: 25%;
+    background: linear-gradient(to bottom, #00d0ea 0%, #0085d1 100%);
+    background-attachment: fixed;
+
+    &:before {
+      content: '';
+      position: absolute;
+      z-index: 0;
+      bottom: 0;
+      right: -8px;
+      height: 20px;
+      width: 20px;
+      background: linear-gradient(to bottom, #00d0ea 0%, #0085d1 100%);
+      background-attachment: fixed;
+      border-bottom-left-radius: 15px;
+    }
+
+    &:after {
+      content: '';
+      position: absolute;
+      z-index: 1;
+      bottom: 0;
+      right: -10px;
+      width: 10px;
+      height: 20px;
+      background: white;
+      border-bottom-left-radius: 10px;
+    }
+  }
+`;
+
+interface MessageProps {
+  person: 'pedro' | 'audience-member' | string;
+  component: () => JSX.Element;
+  key: number;
+}
+
+const Message: React.FC<MessageProps> = ({
+  person,
+  component: Component,
+  key,
+}) => {
+  const bubbleType = person === 'pedro' ? 'mine' : 'yours';
+  return (
+    <StyledDiv className={bubbleType} key={key} id={`${key + 1}`}>
+      <Component />
+    </StyledDiv>
+  );
+};
+
+export default Message;
--- src/pages/presentation.tsx
+++ src/pages/presentation.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import conversation from '../data/script';
 import styled from 'styled-components';
+import Message from '../components/message';
 
-const AllConversation = conversation.map(
-  ({ person, component: Component }, i) => {
-    const bubbleType = person === 'pedro' ? 'mine' : 'yours';
-    return (
-      <div className={`message ${bubbleType}`} key={i}>
-        <Component />
-      </div>
-    );
-  },
-);
+const AllConversation = conversation.map(({ person, component }, key) => (
+  <Message key={key} person={person} component={component} />
+));
 
 const StyledMain = styled.main`
   article {
@@ -22,80 +16,12 @@ const StyledMain = styled.main`
     padding: 10px;
     margin: 0 auto;
   }
-
-  .message {
-    border-radius: 20px;
-    padding: 8px 15px;
-    margin-top: 5px;
-    margin-bottom: 5px;
-    position: relative;
-  }
-
-  .yours {
-    margin-right: 25%;
-    background-color: #eee;
-    &:before {
-      content: '';
-      position: absolute;
-      z-index: 0;
-      bottom: 0;
-      left: -7px;
-      height: 20px;
-      width: 20px;
-      background: #eee;
-      border-bottom-right-radius: 15px;
-    }
-    &::after {
-      content: '';
-      position: absolute;
-      z-index: 1;
-      bottom: 0;
-      left: -10px;
-      width: 10px;
-      height: 20px;
-      background: white;
-      border-bottom-right-radius: 10px;
-    }
-  }
-
-  .mine {
-    align-self: end;
-    color: white;
-    margin-left: 25%;
-    background: linear-gradient(to bottom, #00d0ea 0%, #0085d1 100%);
-    background-attachment: fixed;
-
-    &:before {
-      content: '';
-      position: absolute;
-      z-index: 0;
-      bottom: 0;
-      right: -8px;
-      height: 20px;
-      width: 20px;
-      background: linear-gradient(to bottom, #00d0ea 0%, #0085d1 100%);
-      background-attachment: fixed;
-      border-bottom-left-radius: 15px;
-    }
-
-    &:after {
-      content: '';
-      position: absolute;
-      z-index: 1;
-      bottom: 0;
-      right: -10px;
-      width: 10px;
-      height: 20px;
-      background: white;
-      border-bottom-left-radius: 10px;
-    }
-  }
 `;
 
 const PresentationPage: React.FC = () => {
   return (
     <StyledMain>
-      <h1>Presentation</h1>
+      {/* <h1>Presentation</h1> */}
 
       <article>{AllConversation}</article>
     </StyledMain>
