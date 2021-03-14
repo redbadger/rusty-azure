ddca9d3ba1c0233995b3ab1c5237071776a47127
Pedro Martin <pedro.martin@red-badger.com>
2021-03-14 08:16:58 +0000

    Draft typing indicator

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -74,6 +74,85 @@ const StyledDiv = styled.div`
       border-bottom-left-radius: 10px;
     }
   }
+  /* Typing indicator */
+
+  .typing-indicator {
+    background-color: #e6e7ed;
+    will-change: transform;
+    width: auto;
+    border-radius: 50px;
+    padding: 20px;
+    /* display: table;
+    margin: 0 auto; */
+    position: relative;
+    animation: 2s bulge infinite ease-out;
+    display: flex;
+  }
+  .typing-indicator span {
+    height: 15px;
+    width: 15px;
+    /* float: left; */
+    margin: 0 1px;
+    background-color: #9e9ea1;
+    display: block;
+    border-radius: 50%;
+    opacity: 0.4;
+  }
+  .typing-indicator span:nth-of-type(1) {
+    animation: 1s blink infinite 0.3333s;
+  }
+  .typing-indicator span:nth-of-type(2) {
+    animation: 1s blink infinite 0.6666s;
+  }
+  .typing-indicator span:nth-of-type(3) {
+    animation: 1s blink infinite 0.9999s;
+  }
+
+  @keyframes blink {
+    50% {
+      opacity: 1;
+    }
+  }
+  @keyframes bulge {
+    50% {
+      transform: scale(1.05);
+    }
+  }
+  &.yours .typing-indicator::before,
+  &.yours .typing-indicator::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    left: -2px;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background-color: #e6e7ed;
+  }
+  &.yours .typing-indicator::after {
+    height: 10px;
+    width: 10px;
+    left: -10px;
+    bottom: -10px;
+  }
+
+  &.mine .typing-indicator::before,
+  &.mine .typing-indicator::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    right: -2px;
+    height: 20px;
+    width: 20px;
+    border-radius: 50%;
+    background-color: #e6e7ed;
+  }
+  &.mine .typing-indicator::after {
+    height: 10px;
+    width: 10px;
+    right: -10px;
+    bottom: -10px;
+  }
 `;
 
 interface MessageProps extends IMessage {
@@ -90,6 +169,11 @@ const Message: React.FC<MessageProps> = ({
   const bubbleType = person === 'pedro' ? 'mine' : 'yours';
   return (
     <StyledDiv className={bubbleType} id={`${id + 1}`}>
+      <div className="typing-indicator">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
       <Component />
     </StyledDiv>
   );
