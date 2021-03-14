f52c8a94d74b4191a43c5dc0e995b218f0fec797
Pedro Martin <pedro.martin@red-badger.com>
2021-03-14 09:26:59 +0000

    Abstract typing indicator

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { IMessage } from '../data/script';
+import TypingIndicator from './typing-indicator';
 
 const StyledDiv = styled.div`
   border-radius: 20px;
@@ -76,55 +77,6 @@ const StyledDiv = styled.div`
   }
 `;
 
-const StyledTypingIndicator = styled.div`
-  & {
-    background-color: #e6e7ed;
-    will-change: transform;
-    width: auto;
-    border-radius: 50px;
-    padding: 15px;
-    position: relative;
-    animation: 2s bulge infinite ease-out;
-    display: inline-flex;
-  }
-  & span {
-    height: 15px;
-    width: 15px;
-    margin: 0 1px;
-    background-color: #9e9ea1;
-    display: block;
-    border-radius: 50%;
-    opacity: 0.4;
-  }
-  & span:nth-of-type(1) {
-    animation: 1s blink infinite 0.3333s;
-  }
-  & span:nth-of-type(2) {
-    animation: 1s blink infinite 0.6666s;
-  }
-  & span:nth-of-type(3) {
-    animation: 1s blink infinite 0.9999s;
-  }
-
-  @keyframes blink {
-    50% {
-      opacity: 1;
-    }
-  }
-  @keyframes bulge {
-    50% {
-      transform: scale(1.05);
-    }
-  }
-
-  &.yours {
-    align-self: start;
-  }
-  &.mine {
-    align-self: end;
-  }
-`;
-
 interface MessageProps extends IMessage {
   person: 'pedro' | 'audience-member' | string;
   component: () => JSX.Element;
@@ -151,11 +103,7 @@ const Message: React.FC<MessageProps> = ({
           <Component />
         </StyledDiv>
       ) : (
-        <StyledTypingIndicator className={bubbleType}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </StyledTypingIndicator>
+        <TypingIndicator bubbleType={bubbleType} />
       )}
     </>
   );
