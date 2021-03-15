e2659e1b21c43606af93c5f3c31773a32439dddd
Pedro Martin <pedro.martin@red-badger.com>
2021-03-15 10:17:26 +0000

    Draft chat abstraction

--- src/pages/presentation.tsx
+++ src/pages/presentation.tsx
@@ -16,10 +16,52 @@ const StyledMain = styled.main`
     flex-direction: column;
     width: 400px;
     border: solid 1px #eee;
-    padding: 10px;
     margin: 0 auto;
     justify-content: flex-end;
     min-height: calc(100vh - (1.25rem * 2));
+
+    header {
+      width: 400px;
+      position: fixed;
+      top: 0;
+      z-index: 10;
+      background-color: rgba(244, 244, 244, 0.98);
+      border-bottom: solid 1px #eee;
+      display: flex;
+      padding: 10px;
+      align-items: center;
+      .arrow {
+        display: block;
+        border-left: 2px solid #0085d1;
+        border-bottom: 2px solid #0085d1;
+        min-width: 16px;
+        transform: rotate(45deg);
+        height: 16px;
+      }
+
+      div {
+        display: inline-flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        p {
+          font-size: 0.625rem;
+        }
+      }
+
+      picture {
+        max-width: 12.5%;
+        border-radius: 50%;
+      }
+    }
+
+    .conversation-wrapper {
+      padding: 10px;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+    }
   }
 `;
 
@@ -44,26 +86,19 @@ const PresentationPage: React.FC = () => {
     }
   };
 
-  useEffect(() => {
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: 'smooth',
-    });
-  });
-
   return (
     <StyledMain onKeyDown={handleOnKeyDown} tabIndex={0}>
       <article>
         <header>
-          <Link to="/" />
+          <Link to="/" className="arrow" />
           <div>
             <picture>
               <Ferris />
             </picture>
-            <p></p>
+            <p>Audience member</p>
           </div>
         </header>
-        {Conversation()}
+        <div className="conversation-wrapper">{Conversation()}</div>
       </article>
     </StyledMain>
   );
